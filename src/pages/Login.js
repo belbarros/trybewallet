18ee8450c8120e24eb7c6406a0e1b279a0304729
Izabel de Barros <izabelfeigel@gmail.com>
2022-07-28 17:39:59 -0300

    Req 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,91 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { loginAction } from '../redux/actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      isBtnDisabled: true,
+    };
+  }
+
+  handleInput = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => { this.handleBtn(); });
+  };
+
+  // https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
+  handleBtn = () => {
+    const { email, password } = this.state;
+    const minPass = 6;
+    const regex = /\S+@\S+\.\S+/;
+    if (password.length >= minPass && regex.test(email)) {
+      this.setState({
+        isBtnDisabled: false,
+      }, () => {});
+    } else {
+      this.setState({
+        isBtnDisabled: true,
+      });
+    }
+  };
+
+  onSubmit = (e) => {
+    const { history, login } = this.props;
+    const { email } = this.state;
+    // action aqui
+    login(email);
+    e.preventDefault();
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password, isBtnDisabled } = this.state;
+    return (
+      <form>
+        <input
+          data-testid="email-input"
+          type="text"
+          name="email"
+          value={ email }
+          onChange={ this.handleInput }
+        />
+        <br />
+        <input
+          data-testid="password-input"
+          type="password"
+          name="password"
+          value={ password }
+          onChange={ this.handleInput }
+        />
+        <br />
+        <button
+          type="button"
+          disabled={ isBtnDisabled }
+          onClick={ this.onSubmit }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  login: (email) => dispatch(loginAction(email)),
+});
+
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,17 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+
+const initialState = {
+  currencies: [], // array de string
+  expenses: [], // array de objetos, com cada objeto tendo as chaves id, value, currency, method, tag, description e exchangeRates
+  editor: false, // valor booleano que indica se uma despesa esta sendo editada
+  idToEdit: 0, // valor numérico que armazena id da despesa sendo editada
+};
+
+function wallet(state = initialState, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
+
+export default wallet;
