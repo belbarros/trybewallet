e3f46824365f496ef317fddedfa47a2eeb526723
Izabel de Barros <izabelfeigel@gmail.com>
2022-07-28 20:12:32 -0300

    Preparação pra continuar a 3 amanhã o/

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,9 +1,114 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import { fetchAPI } from '../redux/actions';
+// import PropTypes from 'prop-types';
 
-export default class WalletForm extends Component {
+class WalletForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: '',
+      description: '',
+      method: '',
+      tag: '',
+    };
+  }
+
+  // componentDidMount() {
+  // const { getCurrencies } = this.props;
+  // getCurrencies();
+  // }
+
+  handleInput = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { value, description, method, tag } = this.state;
+    // const { currencies } = this.props;
     return (
-      <div>WalletForm</div>
+      <div>
+        <form>
+          <label htmlFor="value">
+            Valor da despesa:
+            <input
+              data-testid="value-input"
+              type="text"
+              name="value"
+              value={ value }
+              onChange={ this.handleInput }
+            />
+          </label>
+          <br />
+          <label htmlFor="description">
+            Descrição da despesa:
+            <input
+              data-testid="description-input"
+              type="text"
+              name="description"
+              value={ description }
+              onChange={ this.handleInput }
+            />
+          </label>
+          <br />
+          {/* Currencies -> Map do estado global removendo USDT */}
+          <br />
+          <label htmlFor="method">
+            Método de pagamento:
+            <select
+              data-testid="method-input"
+              name="method"
+              value={ method }
+              onChange={ this.handleInput }
+            >
+              <option value="dinheiro">Dinheiro</option>
+              <option value="cartao-credito">Cartão de crédito</option>
+              <option value="cartao-debito">Cartão de débito</option>
+            </select>
+          </label>
+          <br />
+          <label htmlFor="tag">
+            Método de pagamento:
+            <select
+              data-testid="tag-input"
+              name="tag"
+              value={ tag }
+              onChange={ this.handleInput }
+            >
+              <option value="alimentacao">Alimentação</option>
+              <option value="lazer">Lazer</option>
+              <option value="trabalho">Trabalho</option>
+              <option value="transporte">Transporte</option>
+              <option value="saude">Saúde</option>
+            </select>
+          </label>
+          <br />
+          <button
+            type="button"
+            // onClick={}
+          >
+            Adicionar despesa
+          </button>
+        </form>
+      </div>
     );
   }
 }
+
+const mapStateToProps = (store) => ({
+  currencies: store.wallet.currencies,
+});
+
+// const mapDispatchToProps = (dispatch) => ({
+// getCurrencies: () => dispatch(fetchAPI())
+// });
+
+// WalletForm.propTypes = {
+// getCurrencies: PropTypes.func.isRequired,
+// currencies: PropTypes.arrayOf(PropTypes.string).isRequired
+// };
+
+export default connect(mapStateToProps, null)(WalletForm);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,7 +1,38 @@
 // Coloque aqui suas actions
 export const LOGIN = 'LOGIN';
+export const REQUEST_API = 'REQUEST_API';
+export const RECEIVE_API_SUCESS = 'RECEIVE_API_SUCESS';
+export const RECEIVE_API_FAILURE = 'RECEIVE_API_FAILURE';
 
 export const loginAction = (email) => ({
   type: LOGIN,
   payload: email,
 });
+
+export const requestAPI = () => ({
+  type: REQUEST_API,
+});
+
+export const receiveSucess = (currencies) => ({
+  type: RECEIVE_API_SUCESS,
+  currencies,
+});
+
+export const receiveFailure = (error) => ({
+  type: RECEIVE_API_FAILURE,
+  error,
+});
+
+export function fetchAPI() {
+  return async (dispatch) => {
+    dispatch(requestAPI());
+    try {
+      const request = await fetch('https://economia.awesomeapi.com.br/json/all');
+      const response = await request.json();
+      // Retirar apenas as currencies (KEYS)
+      dispatch(receiveSucess(response));
+    } catch (error) {
+      dispatch(receiveFailure(error));
+    }
+  };
+}
