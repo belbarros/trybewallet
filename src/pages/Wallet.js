decb4ab3f694cce3121ba2adb3adebe1510a26dc
Izabel de Barros <izabelfeigel@gmail.com>
2022-07-29 19:35:26 -0300

    Req 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import Table from '../components/Table';
 import WalletForm from '../components/WalletForm';
 
 class Wallet extends React.Component {
@@ -12,6 +13,7 @@ class Wallet extends React.Component {
         </div>
         <div>
           <WalletForm />
+          <Table />
         </div>
       </div>
     );
