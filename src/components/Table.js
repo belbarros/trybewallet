d6c65ce3b5cbb5d17210b9bf75c85639e1f8f3cd
Izabel de Barros <izabelfeigel@gmail.com>
2022-07-29 20:46:42 -0300

    Req 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Table extends Component {
+class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div>
         <table>
@@ -16,8 +19,55 @@ export default class Table extends Component {
             <th>Moeda de conversão</th>
             <th>Editar/Excluir</th>
           </tr>
+          {
+            expenses.map((e) => (
+              <tbody key={ e.id }>
+                <tr>
+                  <td>{ e.description }</td>
+                  <td>{ e.tag }</td>
+                  <td>{ e.method }</td>
+                  <td>{ parseFloat(e.value).toFixed(2) }</td>
+                  <td>{ e.exchangeRates[e.currency].name }</td>
+                  <td>{ parseFloat(e.exchangeRates[e.currency].ask).toFixed(2) }</td>
+                  <td>
+                    { parseFloat(
+                      (e.value
+                    * (e.exchangeRates[e.currency].ask)).toFixed(2),
+                    ) }
+                  </td>
+                  <td>Real</td>
+                  <td>
+                    <button
+                      type="button"
+                      data-testid="edit-btn"
+                      // onClick={}
+                    >
+                      Editar
+                    </button>
+                    <button
+                      type="button"
+                      data-testid="delete-btn"
+                      // onClick={}
+                    >
+                      Excluir
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            ))
+          }
         </table>
       </div>
     );
   }
 }
+
+const mapStateToProps = (store) => ({
+  expenses: store.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.obj).isRequired,
+};
+
+export default connect(mapStateToProps, null)(Table);
